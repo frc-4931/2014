1dceaadb3dfbf4e21ba35d2023e52512714b4564
Randall Hauch <rhauch@gmail.com>
2013-11-13 16:48:46 -0600

    Added two projects for prototype robots. The first is a minimal "basic" robot that doesn't really do anything but is instead just a simple template.
    The second project is a simple robot that uses an arcade-style joystick to drive the robot.

---
+++ PrototypeArcadeDriveRobot/src/org/frc4931/prototype/ArcadeDriveRobot.java
@@ -0,0 +1,83 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008-2013. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.frc4931.prototype;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * An iterative robot that relies upon the command framework for control. The {@link IterativeRobot} superclass will automatically
+ * invoke these methods based upon the current mode of the robot:
+ * <ul>
+ * <li>{@link #robotInit()} - called when the robot is first started up, useful for any initialization code</li>
+ * <li>{@link #autonomousPeriodic()} - called periodically during autonomous mode</li>
+ * <li>{@link #teleopPeriodic()} - called periodically during tele-operation mode</li>
+ * </ul>
+ * <p>
+ * The Virtual Machine is configured to automatically run this class via the "resources/META-INF/MANIFEST.MF" file, which must
+ * contain an entry with the name of this class and the qualified path of this class. For example:
+ * </p>
+ * <code>
+ * MIDlet-Name: ArcadeDriveRobot
+ * ...
+ * MIDlet-1: ArcadeDriveRobot, , org.frc4931.prototype.ArcadeDriveRobot
+ * </code>
+ */
+public class ArcadeDriveRobot extends IterativeRobot {
+
+    public static final class InputPorts {
+        public static final int JOYSTICK = 1;
+    }
+
+    public static final class OutputPorts {
+        public static final int LEFT_MOTOR = 1;
+        public static final int RIGHT_MOTOR = 2;
+    }
+
+    /**
+     * The input for the robot. In this case, the X-axis is selected by default for the turn axis and the Y-axis is selected for
+     * the speed axis.
+     */
+    private Joystick joystick;
+    private RobotDrive drive;
+
+    /**
+     * This function is run when the robot is first started up and should be used for any initialization code.
+     */
+    public void robotInit() {
+        joystick = new Joystick(InputPorts.JOYSTICK);
+        drive = new RobotDrive(OutputPorts.LEFT_MOTOR, OutputPorts.RIGHT_MOTOR);
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    public void autonomousPeriodic() {
+        // does not support autonomous mode
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    public void teleopPeriodic() {
+        while (isOperatorControl() && isEnabled()) {
+            drive.arcadeDrive(joystick);
+            Timer.delay(0.01d);
+        }
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    public void testPeriodic() {
+
+    }
+
+}
