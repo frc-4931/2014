0d33125cd1cce0f1b9bf1e7c7f4270dedaebf6e8
Randall Hauch <rhauch@gmail.com>
2013-12-27 09:23:05 -0600

    Fixed tank-drive robot prototype with correct axes and motor inversion

--- PrototypeTankDriveRobot/src/org/frc4931/prototype/TankDriveRobot.java
+++ PrototypeTankDriveRobot/src/org/frc4931/prototype/TankDriveRobot.java
@@ -10,6 +10,7 @@ package org.frc4931.prototype;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
@@ -32,31 +33,67 @@ import edu.wpi.first.wpilibj.Timer;
  */
 public class TankDriveRobot extends IterativeRobot {
 
-    public static final class InputPorts {
-        public static final int LEFT_JOYSTICK = 1;
-        public static final int RIGHT_JOYSTICK = 1;
+    /**
+     * The drive joystick's input port and button assignments.
+     * <p>
+     * The Logitech controller has the following axis assignments in <em>D MODE</em>:
+     * <ul>
+     * <li>Axis 1 - Left Joystick horizontal (X) axis</li>
+     * <li>Axis 2 - Left Joystick vertical (Y) axis</li>
+     * <li>Axis 3 - Right Joystick horizontal (X) axis</li>
+     * <li>Axis 4 - Right Joystick vertical (Y) axis</li>
+     * <li>Axis 5 - D-Pad Horizontal</li>
+     * <li>Axis 6 - D-Pad Vertical</li>
+     * <ul>
+     * In <em>X MODE</em>:
+     * <ul>
+     * <li>Axis 1 - Left Joystick horizontal (X) axis</li>
+     * <li>Axis 2 - Left Joystick vertical (Y) axis</li>
+     * <li>Axis 3 - Left Trigger</li>
+     * <li>Axis 4 - Right Joystick horizontal (X) axis</li>
+     * <li>Axis 5 - Right Joystick vertical (Y) axis</li>
+     * <li>Axis 6 - Right Trigger</li>
+     * <ul>
+     */
+    public static final class Inputs {
+        public static final int CONTROLLER_PORT = 1;
     }
 
-    public static final class OutputPorts {
-        public static final int LEFT_MOTOR = 1;
-        public static final int RIGHT_MOTOR = 2;
+    public static final class LeftDriveMotor {
+        public static final int JOYSTICK_AXIS = 2; // D-MODE
+        public static final int OUTPUT_PORT = 2;
+        public static final boolean INVERTED = true;
+        public static final MotorType POSITION = MotorType.kRearRight;
+    }
+
+    public static final class RightDriveMotor {
+        public static final int JOYSTICK_AXIS = 4; // D-MODE
+        public static final int OUTPUT_PORT = 1;
+        public static final boolean INVERTED = true;
+        public static final MotorType POSITION = MotorType.kRearLeft;
     }
 
     /**
-     * The input for the robot. In this case, the X-axis is selected by default for the turn axis and the Y-axis is selected for
-     * the speed axis.
+     * The input for the robot. We use separate joysticks here, but really we'd only need on Joystick object (which really
+     * represents the Logitech controller.
      */
-    private Joystick leftJoystick;
-    private Joystick rightJoystick;
+    private Joystick controller;
     private RobotDrive drive;
 
     /**
      * This function is run when the robot is first started up and should be used for any initialization code.
      */
     public void robotInit() {
-        leftJoystick = new Joystick(InputPorts.LEFT_JOYSTICK);
-        rightJoystick = new Joystick(InputPorts.RIGHT_JOYSTICK);
-        drive = new RobotDrive(OutputPorts.LEFT_MOTOR, OutputPorts.RIGHT_MOTOR);
+        // We use separate Joytstick instances since it's actually just one Logitech controller, but we'll actually
+        // use separate but specific axes for the left and right side ...
+        controller = new Joystick(Inputs.CONTROLLER_PORT);
+        drive = new RobotDrive(LeftDriveMotor.OUTPUT_PORT, RightDriveMotor.OUTPUT_PORT);
+        drive.setInvertedMotor(RightDriveMotor.POSITION, RightDriveMotor.INVERTED);
+        drive.setInvertedMotor(LeftDriveMotor.POSITION, LeftDriveMotor.INVERTED);
+    }
+
+    public void autonomousInit() {
+        System.out.println("Entering autonomous mode");
     }
 
     /**
@@ -66,14 +103,30 @@ public class TankDriveRobot extends IterativeRobot {
         // does not support autonomous mode
     }
 
+    public void teleopInit() {
+        System.out.println("Entering teleop mode");
+    }
+
     /**
      * This function is called periodically during operator control.
      */
     public void teleopPeriodic() {
         while (isOperatorControl() && isEnabled()) {
-            drive.tankDrive(leftJoystick, rightJoystick);
+            drive.tankDrive(controller, LeftDriveMotor.JOYSTICK_AXIS, controller, RightDriveMotor.JOYSTICK_AXIS);
+            // drive.tankDrive(leftJoystick, rightJoystick);
             Timer.delay(0.01d);
         }
+    }
+
+    public void disabledInit() {
+        System.out.println("Entering disabled mode");
+    }
+
+    public void disabledPeriodic() {
+    }
+
+    public void testInit() {
+        System.out.println("Entering test mode");
     }
 
     /**
